4658db939b26071e2c12c0f602bf5573981940a0
daniil <kadaniilo@gmail.com>
2018-12-24 14:06:30 +0300

    init

---
+++ sodaMachine.js
@@ -0,0 +1,18 @@
+class SodaMachine {
+  constructor(args={}) {
+    this.sodas = args.sodas;
+    this.cash = args.cash;
+  }
+
+  currentInventoryCount() {
+
+  }
+
+  findSoda(sodaBrand) {
+
+  }
+
+  sell(sodaBrand) {
+
+  }
+}
